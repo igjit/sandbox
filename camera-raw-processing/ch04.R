5b857721c86b456f23075e7c11892fdb2266a1de
igjit <igjit1@gmail.com>
2019-06-18 21:57:01 +0900

    Plot noise

--- camera-raw-processing/ch04.R
+++ camera-raw-processing/ch04.R
@@ -33,3 +33,18 @@ gmm_img <- blc_raw %>%
 gmm_img %>% ta %>% as.cimg %>% plot
 
 gmm_img %>% ta %>% as.cimg %>% imsub(x %inr% c(800, 1000), y %inr% c(1950, 2150)) %>% plot(interpolate = FALSE)
+
+patches <- data.frame(y = c(2086, 2092, 2090, 2090, 2086, 2094, 2096, 2090, 2090, 2086, 2084, 2084,
+                            482, 480, 476, 474, 470, 466, 462, 460, 452, 452, 448, 442) + 1,
+                      x = c(2586, 2430, 2272, 2112, 1958, 1792, 1642, 1486, 1328, 1172, 1016, 860,
+                            866, 1022, 1172, 1328, 1480, 1634, 1788, 1944, 2110, 2266, 2424, 2586) + 1)
+
+noise_df <- patches %>% pmap_dfr(function(y, x) {
+  list(c(0, 1, 0, 1), c(0, 0, 1, 1)) %>% pmap_dfr(function(dx, dy) {
+    v <- blc_raw[seq(y + dy, y + 100, 2), seq(x + dx, x + 100, 2)] %>% as.vector
+    list(mean = mean(v), var = var(v))
+  })
+})
+
+ggplot(noise_df, aes(x = mean, y = var)) +
+  geom_point()
