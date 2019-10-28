1496f0a024d1990891a7d82f5188e307488cb43a
igjit <igjit1@gmail.com>
2019-10-28 12:45:07 +0900

    Test println

--- jvm/tests/testthat/test-jvm.R
+++ jvm/tests/testthat/test-jvm.R
@@ -6,3 +6,8 @@ test_that("Stack works", {
   expect_equal(stack$pop(), "bar")
   expect_equal(stack$pop(), "foo")
 })
+
+test_that("println works", {
+  expect_output(System.out[["println"]]("foo"), "foo")
+  expect_output(System.out$println("bar"), "bar")
+})
