18cde99b29daa9c978cd16e524e789ca1fbdee5e
igjit <igjit1@gmail.com>
2020-01-16 14:21:42 +0900

    kotlin-for-py 07

---
+++ kotlin-for-py/07-collections.kt
@@ -0,0 +1,11 @@
+val strings = listOf("Anne", "Karen", "Peter")
+val map = mapOf("a" to 1, "b" to 2, "c" to 3)
+val set = setOf("a", "b", "c")
+
+val strings = mutableListOf("Anne", "Karen", "Peter")
+val map = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+val set = mutableSetOf("a", "b", "c")
+
+val noInts: List<Int> = listOf()
+val noStrings = listOf<String>()
+val emptyMap = mapOf<String, Int>()
