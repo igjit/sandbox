834a3f2426bed1408318f407d9d226b81d54b72e
igjit <igjit1@gmail.com>
2020-02-01 15:03:10 +0900

    Update chap01.hs

--- learn-you-a-haskell/chap01.hs
+++ learn-you-a-haskell/chap01.hs
@@ -9,3 +9,17 @@ doubleSmallNumber x = if x > 100
                       else x * 2
 
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+-- 1.5 リスト内包表記
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+-- 1.6 タプル
+
+-- let rightTriangles = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2]
+
+-- let rightTriangles' = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a+b+c == 24]
