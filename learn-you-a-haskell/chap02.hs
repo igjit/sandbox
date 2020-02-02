81bca7ae0c14133af291dbd41e641edb68a68215
igjit <igjit1@gmail.com>
2020-02-02 23:24:14 +0900

    Add chap02.hs

---
+++ learn-you-a-haskell/chap02.hs
@@ -0,0 +1,10 @@
+-- 2.2 一般的なHaskellの型
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
