fd9fc0caf614119fc210dfe76d9486a0ce6a613c
igjit <igjit1@gmail.com>
2020-02-05 13:55:15 +0900

    Update chap03.hs

--- learn-you-a-haskell/chap03.hs
+++ learn-you-a-haskell/chap03.hs
@@ -59,3 +59,28 @@ a `myCompare` b
   | a == b = EQ
   | a <= b = LT
   | otherwise = GT
+
+-- 3.3 where?!
+
+bmiTell'' :: Double -> Double -> String
+bmiTell'' weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        skinny = 18.5
+        normal = 25.0
+        fat = 30.0
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+  where (f:_) = firstname
+        (l:_) = lastname
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where bmi weight height = weight / height ^ 2
+
+-- calcBmis [(60, 1.7), (80, 1.8)]
+
