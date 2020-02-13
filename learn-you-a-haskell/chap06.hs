b5e0970604b6265d732d9208181976f18ee36432
igjit <igjit1@gmail.com>
2020-02-14 07:16:50 +0900

    Update chap06.hs

--- learn-you-a-haskell/chap06.hs
+++ learn-you-a-haskell/chap06.hs
@@ -2,6 +2,7 @@
 
 import Data.List
 import Data.Char
+import qualified Data.Map as Map
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -37,3 +38,48 @@ digitSum = sum . map digitToInt . show
 
 firstTo40 :: Maybe Int
 firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+-- 6.3 キーから値へのマッピング
+
+phoneBook =
+  [("betty", "555-2938")
+  ,("bonnie", "452-2928")
+  ,("patsy", "493-2928")
+  ,("lucille", "205-2928")
+  ,("wendy", "939-8282")
+  ,("penny", "853-2492")
+  ]
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
+
+-- findKey "betty" phoneBook
+
+phoneBook' = Map.fromList $
+  [("betty", "555-2938")
+  ,("bonnie", "452-2928")
+  ,("patsy", "493-2928")
+  ,("lucille", "205-2928")
+  ,("wendy", "939-8282")
+  ,("penny", "853-2492")
+  ]
+
+-- Map.lookup "betty" phoneBook'
+
+phoneBook'' =
+  [("betty", "555-2938")
+  ,("betty", "342-2492")
+  ,("bonnie", "452-2928")
+  ,("patsy", "493-2928")
+  ,("patsy", "943-2929")
+  ,("patsy", "827-9162")
+  ,("lucille", "205-2928")
+  ,("wendy", "939-8282")
+  ,("penny", "853-2492")
+  ,("penny", "555-2111")
+  ]
+
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap xs = Map.fromListWith (++) $ map (\ (k, v) -> (k, [v])) xs
+
+-- Map.lookup "patsy" $ phoneBookToMap phoneBook''
