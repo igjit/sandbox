6d151a3b540a0a8d938d3621a5625aa2524718e8
igjit <igjit1@gmail.com>
2020-02-17 07:56:41 +0900

    Update chap07.hs

--- learn-you-a-haskell/chap07.hs
+++ learn-you-a-haskell/chap07.hs
@@ -27,3 +27,24 @@ data Car = Car { company :: String
                } deriving (Show)
 
 -- Car { company="Ford", model="Mustang", year=1967 }
+
+-- 7.4 型引数
+
+-- data Maybe a = Nothing | Just a
+
+-- Maybe は型コンストラクタ
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
+
+-- Vector 3 5 8 `vplus` Vector 9 2 8
+-- Vector 3 9 7 `vmult` 10
+-- Vector 4 9 5 `dotProd` Vector 9.0 2.0 4.0
