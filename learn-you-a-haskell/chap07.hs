3e0a1e1cbc83f629638a9e1ab1a7dad67015c686
igjit <igjit1@gmail.com>
2020-02-20 07:26:26 +0900

    Update chap07.hs

--- learn-you-a-haskell/chap07.hs
+++ learn-you-a-haskell/chap07.hs
@@ -96,3 +96,23 @@ lockers = Map.fromList
 
 -- lockerLookup 101 lockers
 -- lockerLookup 100 lockers
+
+-- 7.7 再帰的なデータ構造
+
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+-- 5 `Cons` Empty
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+-- 3 :-: 4 :-: 5 :-: Empty
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+-- a = 3 :-: 4 :-: 5 :-: Empty
+-- b = 6 :-: 7 :-: Empty
+-- a ^++ b
