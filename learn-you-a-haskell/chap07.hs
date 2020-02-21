2bf960303aaebdd8d0b850ba0fa9ee9253a50e92
igjit <igjit1@gmail.com>
2020-02-21 09:04:22 +0900

    Update chap07.hs

--- learn-you-a-haskell/chap07.hs
+++ learn-you-a-haskell/chap07.hs
@@ -116,3 +116,27 @@ Empty ^++ ys = ys
 -- a = 3 :-: 4 :-: 5 :-: Empty
 -- b = 6 :-: 7 :-: Empty
 -- a ^++ b
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+nums = [8,6,4,1,7,3,5]
+numsTree = foldr treeInsert EmptyTree nums
+
+-- 8 `treeElem` numsTree
