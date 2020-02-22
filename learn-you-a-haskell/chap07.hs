4bfb17003f3af5e9de0ca6917e6854cd325e4a0e
igjit <igjit1@gmail.com>
2020-02-23 08:54:27 +0900

    Update chap07.hs

--- learn-you-a-haskell/chap07.hs
+++ learn-you-a-haskell/chap07.hs
@@ -156,3 +156,34 @@ instance Show TrafficLight where
 
 -- Red == Red
 
+-- 7.9 Yes と No の型クラス
+
+class YesNo a where
+  yesno :: a -> Bool
+
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+
+instance YesNo [a] where
+  yesno [] = False
+  yesno _ = True
+
+instance YesNo Bool where
+  yesno = id
+
+instance YesNo (Maybe a) where
+  yesno (Just _ ) = True
+  yesno Nothing = False
+
+instance YesNo (Tree a) where
+  yesno EmptyTree = False
+  yesno _ = True
+
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno _ = True
+
+-- yesno $ length []
+-- yesno "haha"
+-- yesno ""
