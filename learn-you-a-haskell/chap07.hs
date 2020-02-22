a1406140da377ef04cdc847a321a55bf018d7b7b
igjit <igjit1@gmail.com>
2020-02-22 13:22:21 +0900

    Update chap07.hs

--- learn-you-a-haskell/chap07.hs
+++ learn-you-a-haskell/chap07.hs
@@ -140,3 +140,19 @@ nums = [8,6,4,1,7,3,5]
 numsTree = foldr treeInsert EmptyTree nums
 
 -- 8 `treeElem` numsTree
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Yellow = "Yellow light"
+  show Green = "Green light"
+
+-- Red == Red
+
