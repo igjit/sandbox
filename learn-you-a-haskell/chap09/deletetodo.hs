4fed9545ed0f7614feffb9d84d303d36123b215d
igjit <igjit1@gmail.com>
2020-03-03 12:52:11 +0900

    Update chap09

--- learn-you-a-haskell/chap09/deletetodo.hs
+++ learn-you-a-haskell/chap09/deletetodo.hs
@@ -1,6 +1,7 @@
 import System.IO
 import System.Directory
 import Data.List
+import Control.Exception
 
 main = do
   contents <- readFile "todo.txt"
@@ -13,8 +14,12 @@ main = do
   numberString <- getLine
   let number = read numberString
       newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
-  (tempName, tempHandle) <- openTempFile "." "temp"
-  hPutStr tempHandle newTodoItems
-  hClose tempHandle
-  removeFile "todo.txt"
-  renameFile tempName "todo.txt"
+  bracketOnError (openTempFile "." "temp")
+    ( \(tempName, tempHandle) -> do
+        hClose tempHandle
+        removeFile tempName)
+    ( \(tempName, tempHandle) -> do
+        hPutStr tempHandle newTodoItems
+        hClose tempHandle
+        removeFile "todo.txt"
+        renameFile tempName "todo.txt")
