85c9bf7af1e4d2ed2908e9ce0b9f6ca2ebb8380a
igjit <igjit1@gmail.com>
2020-03-06 08:15:15 +0900

    Add chap09.hs

---
+++ learn-you-a-haskell/chap09.hs
@@ -0,0 +1,10 @@
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+  let (firstCoin, newGen) = random gen
+      (secondCoin, newGen') = random newGen
+      (thirdCoin, newGen'') = random newGen'
+  in (firstCoin, secondCoin, thirdCoin)
+
+-- threeCoins (mkStdGen 22)
