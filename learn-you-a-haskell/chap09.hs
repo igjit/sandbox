5edbf86af845c196f664c668b42f4bbe768a80c3
igjit <igjit1@gmail.com>
2020-03-07 09:45:27 +0900

    Update chap09

--- learn-you-a-haskell/chap09.hs
+++ learn-you-a-haskell/chap09.hs
@@ -8,3 +8,13 @@ threeCoins gen =
   in (firstCoin, secondCoin, thirdCoin)
 
 -- threeCoins (mkStdGen 22)
+
+randoms' :: (RandomGen g, Random a) => g -> [a]
+randoms' gen = let (value, newGen) = random gen in value:randoms' newGen
+
+-- take 5 $ randoms' (mkStdGen 11)
+
+-- randomR (1, 6) (mkStdGen 359353)
+
+-- take 10 $ randomRs ('a', 'z') (mkStdGen 3) :: [Char]
+
