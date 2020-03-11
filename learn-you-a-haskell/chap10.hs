6644f393d7ce801f00e4579f2f8a7759ec82d042
igjit <igjit1@gmail.com>
2020-03-12 06:35:07 +0900

    Update chap10

--- learn-you-a-haskell/chap10.hs
+++ learn-you-a-haskell/chap10.hs
@@ -5,6 +5,13 @@ solveRPN = head . foldl foldingFunction [] . words
   where foldingFunction (x:y:ys) "*" = (y * x):ys
         foldingFunction (x:y:ys) "+" = (y + x):ys
         foldingFunction (x:y:ys) "-" = (y - x):ys
+        foldingFunction (x:y:ys) "/" = (y / x):ys
+        foldingFunction (x:y:ys) "^" = (y ** x):ys
+        foldingFunction (x:ys) "ln" = log x:ys
+        foldingFunction xs "sum" = [sum xs]
         foldingFunction xs numberString = read numberString:xs
 
 -- solveRPN "10 4 3 + 2 * -"
+-- solveRPN "2.7 ln"
+-- solveRPN "10 10 10 10 10 sum 4 /"
+-- solveRPN "10 2 ^"
