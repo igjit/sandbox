d82330797b63a79610306d55df808c6d722cdd3b
igjit <igjit1@gmail.com>
2020-03-16 07:52:42 +0900

    Update chap11

--- learn-you-a-haskell/chap11.hs
+++ learn-you-a-haskell/chap11.hs
@@ -5,3 +5,14 @@
 -- fmap (replicate 3) (Right "blah")
 -- fmap (replicate 3) Nothing
 -- fmap (replicate 3) (Left "foo")
+
+-- 11.2 ファンクター則
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMaybe where
+  fmap f CNothing = CNothing
+  fmap f (CJust counter x) = CJust (counter+1) (f x)
+
+-- fmap (++"ha") (CJust 0 "ho")
+-- fmap id (CJust 0 "haha")
