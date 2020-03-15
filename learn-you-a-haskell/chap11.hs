1bc9e2e377c32903aa2cba300a40f236960122fe
igjit <igjit1@gmail.com>
2020-03-15 15:36:32 +0900

    Add chap11

---
+++ learn-you-a-haskell/chap11.hs
@@ -0,0 +1,7 @@
+-- 11.1 帰ってきたファンクター
+
+-- fmap (replicate 3) [1,2,3,4]
+-- fmap (replicate 3) (Just 4)
+-- fmap (replicate 3) (Right "blah")
+-- fmap (replicate 3) Nothing
+-- fmap (replicate 3) (Left "foo")
