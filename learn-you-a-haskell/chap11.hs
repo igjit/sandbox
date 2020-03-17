1d0ab67ab3888f6231a8c397b6420ba41df91203
igjit <igjit1@gmail.com>
2020-03-17 21:21:01 +0900

    Update chap11

--- learn-you-a-haskell/chap11.hs
+++ learn-you-a-haskell/chap11.hs
@@ -16,3 +16,14 @@ instance Functor CMaybe where
 
 -- fmap (++"ha") (CJust 0 "ho")
 -- fmap id (CJust 0 "haha")
+
+-- 11.3 アプリカティブファンクターを使おう
+
+-- let a = fmap (*) [1,2,3,4]
+-- fmap ( \ f -> f 9) a
+
+-- pure (+) <*> Just 3 <*> Just 5
+
+-- (++) <$> Just "johntra" <*> Just "volta"
+
+-- [(*0),(+100),(^2)] <*> [1,2,3]
