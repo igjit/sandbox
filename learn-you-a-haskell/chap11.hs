8e77ba070b01d04071ff4264a151028d891cf7f6
igjit <igjit1@gmail.com>
2020-03-19 13:57:06 +0900

    Update chap11

--- learn-you-a-haskell/chap11.hs
+++ learn-you-a-haskell/chap11.hs
@@ -1,3 +1,5 @@
+import Control.Applicative
+
 -- 11.1 帰ってきたファンクター
 
 -- fmap (replicate 3) [1,2,3,4]
@@ -34,3 +36,12 @@ instance Functor CMaybe where
 
 myAction :: IO String
 myAction = (++) <$> getLine <*> getLine
+
+-- 11.4 アプリカティブの便利な関数
+
+-- liftA2 (:) (Just 3) (Just [4])
+
+-- sequenceA [Just 3, Just 2, Just 1]
+-- sequenceA [(+3),(+2),(+1)] 3
+-- and $ sequenceA [(>4),(<10),odd] 7
+-- sequenceA [getLine, getLine, getLine]
