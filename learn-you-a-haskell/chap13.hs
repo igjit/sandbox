ca6de7e26e9b3a1c611d7b95740c5568e6626f9a
igjit <igjit1@gmail.com>
2020-03-27 03:50:55 +0900

    Update chap13

--- learn-you-a-haskell/chap13.hs
+++ learn-you-a-haskell/chap13.hs
@@ -10,3 +10,27 @@ applyMaybe (Just x) f = f x
 -- Nothing `applyMaybe` \x -> Just (x ++ " :)")
 -- Just 3 `applyMaybe` \x -> if x > 2 then Just x else Nothing
 -- Just 1 `applyMaybe` \x -> if x > 2 then Just x else Nothing
+
+-- 13.3 Monad型クラス
+
+-- return "WHAT" :: Maybe String
+-- Just 9 >>= \x -> return (x*10)
+-- Nothing >>= \x -> return (x*10)
+
+-- 13.4 綱渡り
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Pole
+landLeft n (left, right) = (left + n, right)
+
+landRight :: Birds -> Pole -> Pole
+landRight n (left, right) = (left, right + n)
+
+-- landLeft 2 (0, 0)
+-- landRight (-1) (1, 2)
+
+x -: f = f x
+
+-- (0, 0) -: landLeft 1 -: landRight 1 -: landLeft 2
