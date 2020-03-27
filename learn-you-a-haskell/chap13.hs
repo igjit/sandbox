0498a9c66b7ffa945728305d377ba53c77c0ce86
igjit <igjit1@gmail.com>
2020-03-28 03:14:50 +0900

    Update chap13

--- learn-you-a-haskell/chap13.hs
+++ learn-you-a-haskell/chap13.hs
@@ -22,11 +22,11 @@ applyMaybe (Just x) f = f x
 type Birds = Int
 type Pole = (Birds, Birds)
 
-landLeft :: Birds -> Pole -> Pole
-landLeft n (left, right) = (left + n, right)
+-- landLeft :: Birds -> Pole -> Pole
+-- landLeft n (left, right) = (left + n, right)
 
-landRight :: Birds -> Pole -> Pole
-landRight n (left, right) = (left, right + n)
+-- landRight :: Birds -> Pole -> Pole
+-- landRight n (left, right) = (left, right + n)
 
 -- landLeft 2 (0, 0)
 -- landRight (-1) (1, 2)
@@ -34,3 +34,22 @@ landRight n (left, right) = (left, right + n)
 x -: f = f x
 
 -- (0, 0) -: landLeft 1 -: landRight 1 -: landLeft 2
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just(left + n, right)
+  | otherwise = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs(left - (right + n)) < 4 = Just(left, right + n)
+  | otherwise = Nothing
+
+-- landLeft 2 (0, 0)
+-- landLeft 10 (0, 3)
+
+-- landRight 1 (0, 0) >>= landLeft 2
+-- Nothing >>= landLeft 2
+
+-- return (0, 0) >>= landRight 2 >>= landLeft 2 >>= landRight 2
+-- return (0, 0) >>= landLeft 1 >>= landRight 4 >>= landLeft (-1) >>= landRight (-2)
