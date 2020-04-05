e7718badbf560fd102d33876124c22a96b0af695
igjit <igjit1@gmail.com>
2020-04-05 20:54:02 +0900

    Update chap14

--- learn-you-a-haskell/chap14.hs
+++ learn-you-a-haskell/chap14.hs
@@ -1,5 +1,7 @@
 -- :set -package mtl
 import Control.Monad.Writer
+import Control.Monad.State
+import System.Random
 
 -- 14.1 Writer? 中の人なんていません!
 
@@ -61,3 +63,56 @@ addStuff = do
   a <- (*2)
   b <- (+10)
   return (a+b)
+
+-- 14.3 計算の状態の正体
+
+type Stack = [Int]
+
+-- pop :: Stack -> (Int, Stack)
+-- pop (x:xs) = (x, xs)
+
+-- push :: Int -> Stack -> ((), Stack)
+-- push a xs = ((), a:xs)
+
+-- stackManip :: Stack -> (Int, Stack)
+-- stackManip stack = let
+--   ((), newStack1) = push 3 stack
+--   (a , newStack2) = pop newStack1
+--   in pop newStack2
+
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
+stackManip :: State Stack Int
+stackManip = do
+  push 3
+  pop
+  pop
+
+-- runState stackManip [5,8,2,1]
+
+stackStuff :: State Stack ()
+stackStuff = do
+  a <- pop
+  if a == 5
+    then push 5
+    else do
+      push 3
+      push 8
+
+-- runState stackStuff [9,0,2,1,0]
+
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
+
+threeCoins :: State StdGen (Bool, Bool, Bool)
+threeCoins = do
+  a <- randomSt
+  b <- randomSt
+  c <- randomSt
+  return (a, b, c)
+
+-- runState threeCoins (mkStdGen 33)
